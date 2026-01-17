a7fbba5f08d2fd740c9acbd5e9ab395683e3dba0
ytlikes <gabrielcardosonunes159@gmail.com>
2026-01-17 14:27:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,6 @@ st.markdown("""
     .orb-icon {
         font-size: 60px;
         filter: drop-shadow(0 0 10px rgba(255, 255, 255, 0.5));
-        pointer-events: none;
     }
 
     /* Status text minimalista */
